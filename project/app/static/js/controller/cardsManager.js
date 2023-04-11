40385b58b9141ccb98d10db58e2a11c31de26c2f
piotrpoloczek <piotr.jan.poloczek@gmail.com>
2023-04-11 06:44:12 +0200

    add button arrow to show cards

--- project/app/static/js/controller/cardsManager.js
+++ project/app/static/js/controller/cardsManager.js
@@ -12,7 +12,7 @@ export let cardsManager = {
         for (let card of cards) {
             const cardBuilder = htmlFactory(htmlTemplates.card);
             const content = cardBuilder(card);
-            domManager.addChild(`.div-cards[data-board-id="${boardId}"]`, content);
+            domManager.addChild(`#div-cards[data-board-id="${boardId}"]`, content);
             domManager.addEventListener(
                 `[data-card-id="${card.id}"]`,
                 "click",
