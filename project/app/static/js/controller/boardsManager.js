9c64be23e8ad131c2f2dfb6af9da5692f6633a25
piotrpoloczek <piotr.jan.poloczek@gmail.com>
2023-04-16 15:23:40 +0200

    refactor boardsManager

--- project/app/static/js/controller/boardsManager.js
+++ project/app/static/js/controller/boardsManager.js
@@ -1,9 +1,8 @@
 import { boardsHandler } from "../data/boardsHandler.js";
 import {htmlFactory, htmlTemplates} from "../view/htmlFactory.js";
 import {domManager} from "../view/domManager.js";
-import { columnManager } from "./columnManager.js";
-import { dragManager } from "./dragManager.js";
 import { refreshManager } from "./refreshManager.js";
+import { boardsManagerFunc } from "./boardsManagerFunc.js";
 
 
 export let boardsManager = {
@@ -20,16 +19,16 @@ export let boardsManager = {
                     `button[data-board-id="${board.id}"]`,
                     //`.toggle-board-button[data-board-id="${board.id}"]`,
                     "click",
-                    showHideButtonHandler
+                    boardsManagerFunc.showHideButtonHandler
                 );
                 domManager.addEventListener(
                     `h5#board-header[data-board-id="${board.id}"]`,
                     //`.toggle-board-button[data-board-id="${board.id}"]`,
                     "input",
-                    changeText
+                    boardsManagerFunc.changeText
                 );
 
-                openStaysOpen(openBoardId, board.id);
+                boardsManagerFunc.openStaysOpen(openBoardId, board.id);
 
             }; 
         }
@@ -48,48 +47,3 @@ export let boardsManager = {
         await boardsManager.loadBoards(null, openBoardId);
     },
 };
-
-async function showHideButtonHandler(clickEvent) {
-    const boardId = await clickEvent.currentTarget.dataset.boardId;
-    const currentTargetElement = await clickEvent.currentTarget;
-    const boardElement = document.querySelector(`#div-cards[data-board-id="${boardId}"]`)
-
-    if (currentTargetElement.classList.contains("open")){
-        hideAllBoard(currentTargetElement, boardElement);
-    } else {
-        await showAllBoard(currentTargetElement, boardElement, boardId);
-    }   
-}
-
-async function changeText(changeEvent) {
-    const elementHTML = await changeEvent.currentTarget;
-    console.log(elementHTML)
-}
-
-function hideAllBoard(currentTargetElement, boardElement) {
-    domManager.changeBetweenCSSClasses(currentTargetElement, "open", "closed");
-    domManager.changeBetweenCSSClasses(boardElement, "height-500", "height-0");
-    boardElement.innerHTML = "";
-}
-
-async function showAllBoard(currentTargetElement, boardElement, boardId) {
-    domManager.changeBetweenCSSClasses(currentTargetElement, "closed", "open");
-    domManager.changeBetweenCSSClasses(boardElement, "height-0", "height-500");
-    await columnManager.loadColumn(boardId);
-    dragManager.initDragManager();
-}
-
-async function openStaysOpen(openBoardId, boardId) {
-    const boardElement = document.querySelector(`#div-cards[data-board-id="${boardId}"]`)
-    const currentTargetElement = document.querySelector(`button[data-board-id="${boardId}"]`)
-
-    if (openBoardId != null) {
-        console.log("open boards id: " + openBoardId)
-        console.log(openBoardId);
-        console.log("board id:       " + boardId)
-        if (openBoardId.includes(`${boardId}`)) {
-            console.log("yes includes")
-            await showAllBoard(currentTargetElement, boardElement, boardId);
-        }
-    }
-}
