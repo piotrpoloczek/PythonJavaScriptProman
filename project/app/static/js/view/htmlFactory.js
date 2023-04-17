515720d8cecb0ab709c81f9961902af8ae291f74
piotrpoloczek <piotr.jan.poloczek@gmail.com>
2023-04-17 13:23:34 +0200

    change columnbuilder

--- project/app/static/js/view/htmlFactory.js
+++ project/app/static/js/view/htmlFactory.js
@@ -35,15 +35,19 @@ function columnBuilder(column, isFirst) {
                 <div class="col-sm-4" data-column-id="${column.id}">
                     <div class="card m-1">
                         <div class="row card-header m-0"> 
-                            <div class="col-sm-9">
+                            <div class="col-sm-8">
                                 <h5>${column.title} </h5>
                             </div>
-                            <div class="col-sm-3 div-button" data-column-id="${column.id}">
-                            <button data-toggle="modal" type="button" class="btn btn-primary" >
-                            <span class="bi bi-trash"></span></button>
+                            <div class="col-sm-2 div-button-delete-column" data-board-id="${column.id}" data-column-id="${column.id}">
+                                <button type="button" class="btn btn-primary" >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+                                    </svg>
+                                </button>
                             </div>
-                            <div class="col-sm-3 div-button" data-board-id="${column.id}">
-                            ${buttonaddCard} 
+                            <div class="col-sm-2 div-button-add-card" data-board-id="${column.id}" data-column-id="${column.id}">
+                                ${buttonaddCard} 
                             </div>
                         </div>
                         <div class="card-body" id="cards" data-column-id="${column.id}"></div>
