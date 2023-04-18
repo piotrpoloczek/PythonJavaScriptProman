43e5a8315497daa258ee3cb1e27512a25277d0e9
Piotr <piotr@danielewicz.eu>
2023-04-19 00:30:00 +0200

    updata board title v.1

--- project/api_board/views_boards.py
+++ project/api_board/views_boards.py
@@ -29,13 +29,9 @@ def create_board():
 def delete_board(board_id: int):
     return queries.delete_board(board_id)
 
-@api_board_bp.route("/boards/<int:board_id>", methods=["POST"])
+@api_board_bp.route("/boards/<int:board_id>/updata", methods=["POST"])
 @json_response
 def updata_board(board_id: int):
-    print("board_id")
-    print(board_id)
     board_title = request.get_json()["title"]
-    print("board_id")
-    print(board_title)
     queries.updata_board(board_id,board_title)
     return {"title": board_title, "http_code": 201}
